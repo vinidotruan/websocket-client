1c44de8bf9aed04dfe016512c2179de16591c5c1
Vini <contatoruanvinicius@gmail.com>
2024-12-19 22:14:58 -0300

    env settup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: false,
-  apiUrl: "https://mss.vinidotruan.live/api",
-  authSpa: "https://mss.vinidotruan.live/sanctum/csrf-cookie",
+  apiUrl: "http://localhost:8000/api",
+  authSpa: "http://localhost:8000/sanctum/csrf-cookie",
   pusher: {
     broadcaster: 'pusher',
     key: 'app-key',
-    wsHost: 'localhost',
+    wsHost: 'vinidotruan.live',
     wsPort: '6001',
     wssPort: '6001',
     forceTLS: false,
     enabledTransports: ['ws'],
     cluster: 'mt1',
-    authEndpoint: 'https://mss.vinidotruan.live/broadcasting/auth',
+    authEndpoint: 'http://localhost:8000/broadcasting/auth',
   },
 };
